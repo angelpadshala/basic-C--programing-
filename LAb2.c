6fbb3ba630d48628c5071476f425fdeed04775ed
angel padshala <angelpadshala132@gmail.com>
2025-09-04 00:16:49 +0530

    LAb2

---
+++ LAb2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("ENTER A VALUE FOR a: ");
+    scanf("%d", &a);
+    printf("ENTER A VALUE FOR b: ");
+    scanf("%d", &b);
+    c=a-b;
+    printf("a-b:%d",c);
+    return 0;
+}
+
