b4ec85500f67e893a9cea89bde45716fd44bf974
angel padshala <angelpadshala132@gmail.com>
2025-09-04 00:19:54 +0530

    LAb6

---
+++ LAb6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    float m,h;
+    printf("Enter the time in hours: ");
+    scanf("%2f",&h);
+    m=h*60;
+    printf("THe time in minute=%2f",m);
+    return 0;
+}
