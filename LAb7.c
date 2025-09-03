2d6642b1d9aa9b81be580b9e0a35fa3c2beb0a81
angel padshala <angelpadshala132@gmail.com>
2025-09-04 00:20:30 +0530

    LAb7

---
+++ LAb7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    float m,h;
+     printf("Enter the time in minute: ");
+    scanf("%2f",&m);
+    h=m/60;
+    printf("THe time in hour=%2f",h);
+    return 0;
+}
