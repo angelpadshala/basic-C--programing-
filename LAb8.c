30b58a810a8a9fe99e509757dbcf1f9bc36da982
angel padshala <angelpadshala132@gmail.com>
2025-09-04 00:21:01 +0530

    LAb8

---
+++ LAb8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int d,r;
+    printf("Enter the amount in dollars:");
+    scanf("%d",&d);
+    r=d*48;
+    printf("The amount in Rs=%d",r);
+    return 0;
+}
