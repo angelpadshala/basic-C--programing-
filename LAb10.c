1da426d3c9ef8560382907a781d1b9c594573f1b
angel padshala <angelpadshala132@gmail.com>
2025-09-11 03:17:29 +0530

    pro10.c

---
+++ LAb10.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int d,r,p;
+
+    printf("Enter amount in Dollars: ");
+    scanf("%d", &d);
+
+    r=d*48;
+    p=r*70;
+
+    printf("The amount in pound:%d", p);
+    return 0;
+
+}
+
+
+
+
+
